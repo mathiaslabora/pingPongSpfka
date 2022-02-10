c9d8c74308521529464943b9bddec85b409b4fe8
mathias labora martirena <mathiaslabora@gmail.com>
2022-02-10 19:54:28 -0300

    final

--- main.js
+++ main.js
@@ -8,7 +8,6 @@
         this.ball = null;
         this.playing = false;
     }
-
     self.Board.prototype = {
         get elements() {
             var elements = this.bars.map(function (bar) {
@@ -19,33 +18,30 @@
         }
     }
 })();
-
 //pregunta. uso self y prototype detallado para entenderlo.
 
 (function () {//objeto ball
     self.Ball = function (x, y, radius, board) {
-        this.x = x;
         this.y = y;
+        this.x = x;
         this.radius = radius;
+        this.board = board;
         this.speed_y = 0;
         this.speed_x = 3;
-        this.board = board;
-        this.direction = 1;
         this.bounce_angle = 0;
-        this.max_bounce_angle = Math.PI / 12;
+        this.direction = 1;
         this.speed = 3;
+        this.max_bounce_angle = Math.PI / 12;
 
         board.ball = this;
         this.kind = "circle";
     }
 
-
-//pregunta. diferencias uso get: y funciones
+    //pregunta. diferencias uso get: y funciones
     self.Ball.prototype = {
         move: function () {
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
-
         },
         get width() {
             return this.radius * 2;
@@ -55,7 +51,6 @@
             return this.radius * 2;
 
         },
-
         collision: function (bar) {
             var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
 
@@ -68,35 +63,6 @@
 
             if (this.x > (this.board.width / 2)) this.direction = -1;
             else this.direction = 1;
-
-        }
-
-    }
-
-})();
-
-(function () { //objeto bar
-    self.Bar = function (x, y, width, height, board) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.board = board;
-        this.board.bars.push(this);
-        this.kind = "rectangle";
-        this.speed = 10;
-    }
-
-    self.Bar.prototype = {
-        down: function () {
-            this.y += this.speed;
-
-        },
-        up: function () {
-            this.y -= this.speed;
-        },
-        toString: function () {
-            return "x: " + this.x + "y: " + this.y;
         }
     }
 })();
@@ -109,7 +75,6 @@
         this.board = board;
         this.cxt = canvas.getContext("2d");
     }
-
     self.BoardView.prototype = {
         clean: function () {
             this.cxt.clearRect(0, 0, this.board.width, this.board.height);
@@ -129,7 +94,6 @@
 
                 }
             };
-
         },
         play: function () {
             if (this.board.playing) {
@@ -139,9 +103,7 @@
                 this.board.ball.move();
             }
         }
-
     }
-
     function hit(a, b) {//funcion dentro de boardview
         var hit = false;
         if (b.x + b.width >= a.x && b.x < a.x + a.width) {
@@ -164,7 +126,6 @@
     }
 
     function draw(cxt, element) {//funcion draw dentro de boardview
-
         switch (element.kind) {
             case "rectangle":
                 cxt.fillRect(element.x, element.y, element.width, element.height);
@@ -180,6 +141,31 @@
     }
 })();
 
+(function () { //objeto bar
+    self.Bar = function (x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+        this.speed = 10;
+    }
+    self.Bar.prototype = {
+        down: function () {
+            this.y += this.speed;
+
+        },
+        up: function () {
+            this.y -= this.speed;
+        },
+        toString: function () {
+            return "x: " + this.x + "y: " + this.y;
+        }
+    }
+})();
+
 var board = new Board(800, 400);
 var bar = new Bar(20, 100, 40, 100, board);
 var bar_2 = new Bar(735, 100, 40, 100, board);
@@ -188,33 +174,28 @@ var board_view = new BoardView(canvas, board);
 var ball = new Ball(350, 100, 10, board);
 
 document.addEventListener("keydown", function (ev) {
-
-
     //pregunta. alternativa a keycode ya que hoy dia esta deprecated
     if (ev.keyCode == 38) {
         ev.preventDefault();
-        bar.up();
-
+        bar_2.up();
     }
     else if (ev.keyCode == 40) {
         ev.preventDefault();
-        bar.down();
+        bar_2.down();
     }
     else if (ev.keyCode == 87) {
         ev.preventDefault();
-        bar_2.up();
+        bar.up();
     }
     else if (ev.keyCode == 83) {
         ev.preventDefault();
-        bar_2.down();
+        bar.down();
     }
     else if (ev.keyCode === 32) {
         ev.preventDefault();
         board.playing = !board.playing;
 
     }
-
-
 });
 
 //pregunta. explicacion requestAnimationFrame
@@ -227,5 +208,4 @@ setTimeout(function () {
 function controller() {
     board_view.play();
     window.requestAnimationFrame(controller);
-
 }
