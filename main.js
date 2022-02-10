605f4b1d6e176a9773703f45a0e7597ddb9484bf
mathias labora martirena <mathiaslabora@gmail.com>
2022-02-10 19:35:31 -0300

    agregado objeto bar y obj ball y algunos metodos

--- main.js
+++ main.js
@@ -6,18 +6,100 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
-        
+        this.playing = false;
     }
 
     self.Board.prototype = {
         get elements() {
-            var elements = this.bars;
+            var elements = this.bars.map(function (bar) {
+                return bar;
+            });
             elements.push(this.ball);
             return elements;
         }
     }
 })();
 
+//pregunta. uso self y prototype detallado para entenderlo.
+
+(function () {//objeto ball
+    self.Ball = function (x, y, radius, board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
+
+
+//pregunta. diferencias uso get: y funciones
+    self.Ball.prototype = {
+        move: function () {
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+
+        },
+        get width() {
+            return this.radius * 2;
+
+        },
+        get height() {
+            return this.radius * 2;
+
+        },
+
+        collision: function (bar) {
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
+
+        }
+
+    }
+
+})();
+
+(function () { //objeto bar
+    self.Bar = function (x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+        this.speed = 10;
+    }
+
+    self.Bar.prototype = {
+        down: function () {
+            this.y += this.speed;
+
+        },
+        up: function () {
+            this.y -= this.speed;
+        },
+        toString: function () {
+            return "x: " + this.x + "y: " + this.y;
+        }
+    }
+})();
 
 (function () { //objeto boardview
     self.BoardView = function (canvas, board) {
@@ -27,3 +109,74 @@
         this.board = board;
         this.cxt = canvas.getContext("2d");
     }
+
+    self.BoardView.prototype = {
+        clean: function () {
+            this.cxt.clearRect(0, 0, this.board.width, this.board.height);
+        },
+        draw: function () {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i];
+
+                draw(this.cxt, el);
+            };
+        },
+        check_collisions: function () {
+            for (var i = this.board.bars.length - 1; i >= 0; i--) {
+                var bar = this.board.bars[i];
+                if (hit(bar, this.board.ball)) {
+                    this.board.ball.collision(bar);
+
+                }
+            };
+
+        },
+        play: function () {
+            if (this.board.playing) {
+                this.clean();
+                this.draw();
+                this.check_collisions();
+                this.board.ball.move();
+            }
+        }
+
+    }
+
+    function hit(a, b) {//funcion dentro de boardview
+        var hit = false;
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+
+            if (b.y + b.height >= a.y && b.y < a.y + a.height)
+                hit = true;
+        }
+
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+
+            if (b.y <= a.y && b.y + b.height >= a.y + a.height)
+                hit = true;
+        }
+
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height)
+                hit = true;
+        }
+        return hit;
+    }
+
+    function draw(cxt, element) {//funcion draw dentro de boardview
+
+        switch (element.kind) {
+            case "rectangle":
+                cxt.fillRect(element.x, element.y, element.width, element.height);
+                break;
+
+            case "circle":
+                cxt.beginPath();
+                cxt.arc(element.x, element.y, element.radius, 0, 7);
+                cxt.fill();
+                cxt.closePath();
+                break;
+        }
+    }
+})();
+
