a69b363208d876c26adc1f4bdecb005e66562fda
mathias labora martirena <mathiaslabora@gmail.com>
2022-02-10 19:39:07 -0300

    agregado evento keydown

--- main.js
+++ main.js
@@ -180,3 +180,39 @@
     }
 })();
 
+var board = new Board(800, 400);
+var bar = new Bar(20, 100, 40, 100, board);
+var bar_2 = new Bar(735, 100, 40, 100, board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
+
+document.addEventListener("keydown", function (ev) {
+
+
+    //pregunta. alternativa a keycode ya que hoy dia esta deprecated
+    if (ev.keyCode == 38) {
+        ev.preventDefault();
+        bar.up();
+
+    }
+    else if (ev.keyCode == 40) {
+        ev.preventDefault();
+        bar.down();
+    }
+    else if (ev.keyCode == 87) {
+        ev.preventDefault();
+        bar_2.up();
+    }
+    else if (ev.keyCode == 83) {
+        ev.preventDefault();
+        bar_2.down();
+    }
+    else if (ev.keyCode === 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
+
+    }
+
+
+});
